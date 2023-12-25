3c3f38ea4ceb0f51f39026e2af914b9edc40275e
gesoges0 <elasticnet1995@gmail.com>
2023-12-26 00:16:17 +0900

    fix: loop

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -9,23 +9,29 @@ fn main() {
 
     println!("The secret number is: {}", secret_number);
 
-    println!("Please input your guess!");
 
-    let mut guess = String::new();
+    loop{
+        println!("Please input your guess!");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        let mut guess = String::new();
 
-    // 型変換
-    let guess: u32 =  guess.trim().parse()
-        .expect("Please type a number");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    println!("You guessed: {}", guess);
+        // 型変換
+        let guess: u32 =  guess.trim().parse()
+            .expect("Please type a number");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
